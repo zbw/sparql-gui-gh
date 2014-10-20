163b237904cc46dc9e0cb3bf958915657f88554d
Joachim Neubert <j.neubert@zbw.eu>
2014-10-20 10:00:49 +0200

    Formatting and default arguments

--- yasqeInit.js
+++ yasqeInit.js
@@ -1,31 +1,38 @@
 var consumeUrl = function(yasqe, args) {
-//change query and endpoint value if there are any
-// "https://raw.githubusercontent.com/jneubert/skos-history/master/sparql/version_overview.rq"
-if (args.query) yasqe.setValue(args.query);
-if (args.endpoint) yasqe.options.sparql.endpoint = "http://zbw.eu/beta/sparql/stwv/query";
 
-//want to consume other arguments such as the request type (POST/GET), or arguments to send to endpoint
-//feel free to add them in this function as well.
-//as you see, all options you can specify in the default settings, are configurable via yasqe.options as well
- 
-//Or, if you want to configure yasqe via a remote url (e.g. a query in some file elsewhere),
-//feel free to do so!
-//This example uses a cors proxy to access a github file containing a query
-if (args.queryRef) $.get(args.queryRef, function(data) {yasqe.setValue(atob(data.content))});
+  //change query and endpoint value if there are any
+  if (args.query) yasqe.setValue(args.query);
+  if (args.endpoint) {
+    yasqe.options.sparql.endpoint = args.endpoint;
+  } else {
+    yasqe.options.sparql.endpoint = "http://zbw.eu/beta/sparql/stwv/query";
+  }
+
+  //want to consume other arguments such as the request type (POST/GET), or arguments to send to endpoint
+  //feel free to add them in this function as well.
+  //as you see, all options you can specify in the default settings, are configurable via yasqe.options as well
+
+  //Or, if you want to configure yasqe via a remote url (e.g. a query in some file elsewhere),
+  //feel free to do so!
+  //This example uses a cors proxy to access a github file containing a query
+  if (args.queryRef) {
+    $.get(args.queryRef, function(data) {yasqe.setValue(atob(data.content))});
+  } else {
+    $.get("https://api.github.com/repos/jneubert/skos-history/contents/sparql/version_overview.rq", function(data) {yasqe.setValue(atob(data.content))});
+  }
 };
  
 var yasqe = YASQE(document.getElementById("yasqe"), {
-sparql: {
-showQueryButton: true,
-endpoint: "http://zbw.eu/beta/sparql/stwv/query"
-},
-consumeShareLink: consumeUrl
+  sparql: {
+    showQueryButton: true,
+  },
+  consumeShareLink: consumeUrl
 });
 
 
 var yasr = YASR(document.getElementById("yasr"), {
-//this way, the URLs in the results are prettified using the defined prefixes in the query
-getUsedPrefixes: yasqe.getPrefixesFromQuery
+  //this way, the URLs in the results are prettified using the defined prefixes in the query
+  getUsedPrefixes: yasqe.getPrefixesFromQuery
 });
  
 /**
@@ -37,7 +44,7 @@ yasr.setResponse({response: data, contentType: xhr.getResponseHeader("Content-Ty
 yasqe.options.sparql.handlers.error = function(xhr, textStatus, errorThrown) {
 var exceptionMsg = textStatus + " (response status code " + xhr.status + ")";
 if (errorThrown && errorThrown.length) exceptionMsg += ": " + errorThrown;
-yasr.setResponse({exception: exceptionMsg});
+  yasr.setResponse({exception: exceptionMsg});
 };
 
 
