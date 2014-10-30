6a40740a6ee0464ce12b0d2a1ed01980c9e30f4c
Joachim Neubert <j.neubert@zbw.eu>
2014-10-30 09:20:48 +0100

    Update to yasqe 1.5.4/yasr 1.2.3

--- update_yas.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-#wget --no-check-certificate -q -O yasqe.bundled.min.js https://raw.githubusercontent.com/YASGUI/YASQE/gh-pages/dist/yasqe.bundled.min.js
-#wget --no-check-certificate -q -O yasqe.min.css https://raw.githubusercontent.com/YASGUI/YASQE/gh-pages/dist/yasqe.min.css
-wget --no-check-certificate -q -O yasr.bundled.min.js https://raw.githubusercontent.com/YASGUI/YASR/gh-pages/dist/yasr.bundled.min.js
-wget --no-check-certificate -q -O yasr.min.css https://raw.githubusercontent.com/YASGUI/YASR/gh-pages/dist/yasr.min.css
-
