e18efa06208fc9f05c791dea3520502b593c4a2f
Joachim Neubert <j.neubert@zbw.eu>
2014-11-14 11:33:11 +0100

    Work with 'query' url param as well as with 'queryRef'

--- sparql-lab.js
+++ sparql-lab.js
@@ -2,20 +2,23 @@
 var consumeUrl = function(yasqe, args) {
 
   //change query and endpoint value if there are any
-  if (args.query) yasqe.setValue(args.query);
+  if (args.query) {
+    yasqe.setValue(args.query);
+    yasqe.query();
+  }
   if (args.endpoint) {
     yasqe.options.sparql.endpoint = args.endpoint;
   } else {
     yasqe.options.sparql.endpoint = "http://zbw.eu/beta/sparql/stwv/query";
   }
+  // display the endpoint url on the page
+  document.getElementById("endpoint_url").innerHTML = yasqe.options.sparql.endpoint;
   if (args.hide) {
     document.getElementById("yasqe").style.display = "none";
     document.getElementById("results").style.display = "none";
     document.getElementById("results-link").style.display = "none";
     YASR.defaults.drawOutputSelector = false;
   }
-  // display the endpoint url on the page
-  document.getElementById("endpoint_url").innerHTML = yasqe.options.sparql.endpoint;
 
   //want to consume other arguments such as the request type (POST/GET), or arguments to send to endpoint
   //feel free to add them in this function as well.
@@ -24,24 +27,21 @@ var consumeUrl = function(yasqe, args) {
   //Or, if you want to configure yasqe via a remote url (e.g. a query in some file elsewhere),
   //feel free to do so!
   //This example uses a cors proxy to access a github file containing a query
-  var queryReferenceURI = function () {
-    if (args.queryRef) {
-      return args.queryRef;
-    } else {
-      return "https://api.github.com/repos/jneubert/skos-history/contents/sparql/version_overview.rq";
-    }
-  }
-  // display query reference
-  var re = new RegExp("https://api.github.com/repos/(.*?)/contents/(.*)");
-  var found = queryReferenceURI().match(re);
-  document.getElementById("repo").innerHTML = found[1];
-  document.getElementById("query_reference").innerHTML = found[2];
+  if (args.queryRef) {
 
-  // get the query and exexcute
-  $.get(queryReferenceURI(), function(data) {
-    yasqe.setValue(atob(data.content));
-    yasqe.query();
-  });
+    // display query reference
+    var re = new RegExp("https://api.github.com/repos/(.*?)/contents/(.*)");
+    var query_uri = args.queryRef;
+    var found = query_uri.match(re);
+    document.getElementById("repo").innerHTML = found[1];
+    document.getElementById("query_reference").innerHTML = found[2];
+
+    // get the query and exexcute
+    $.get(args.queryRef, function(data) {
+      yasqe.setValue(atob(data.content));
+      yasqe.query();
+    });
+  }
 };
  
 var yasqe = YASQE(document.getElementById("yasqe"), {
