b451a685a6230d0717023c2f969b63f3e58d97f1
Joachim Neubert <j.neubert@zbw.eu>
2017-01-04 09:11:41 +0100

    interim

--- show_result.js
+++ show_result.js
@@ -25,27 +25,54 @@ var yasr = YASR(document.getElementById("yasr"), {
 });
 
 // main function
-var loadSparqlResult = function(args) {
-  var reportFileName, downloadLink;
+var loadResultFile = function(args) {
+  var pageVars = {};
 
-  // get datafile name
-  reportFileName = document.getElementById("jsonFile").href;
+  if (args) {
 
-  // load datafile
-  jQuery.getJSON(reportFileName, function (data) {
-    window.yasr.setResponse(data);
+    if (args.resultRef) {
+      var re, resultFile;
 
-    // execute search from parameter
-    if (getParameterByName('search') !== '') {
-      YASR.$('.dataTables_filter input').val(getParameterByName('search')).keyup();
+      // distinguish by host name -
+      // get the host name / first part of the URL
+      switch (args.resultRef.match("^http[s]?://([a-zA-Z0-9_\\.-]+?)/.*")[1]) {
+        // IIPT repository - available only within the ZBW intranet
+        case 'ite-git':
+          queryHost = 'IIPT';
+          re = new RegExp("http://ite-git/gitlist/(.*?)\\.git/raw/master/(.*)");
+          break;
+        // GitHub repository - use a cors proxy to access a github file
+        case 'api.github.com':
+          queryHost = 'GitHub';
+          re = new RegExp("https://api.github.com/repos/(.*?)/contents/(.*)");
+          break;
+      }
+
+      // set variables for use in HTML page
+      pageVars.resultRef = args.resultRef;
+
+      // get and show the result
+      jQuery.getJSON(resultRef, function (data) {
+        var result;
+
+        // a repository may return the result directly, or,
+        // in case of Github, as a JSON data structure with encoded content
+        if (queryHost === 'GitHub') {
+          result = atob(data.content);
+        } else {
+          result = data;
+        }
+        window.yasr.setResponse(result);
+      });
     }
-  });
+  }
+  // set more variables for use in HTML page, and modify it
+  setPageVars(pageVars);
 };
 
-// helper function
-function getParameterByName(name) {
-  name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
-  var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
-    results = regex.exec(location.search);
-  return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+// set specific elements on the html page
+function setPageVars (vars) {
+  // endpoint has to be defined
+  document.getElementById("result_file").innerHTML = vars.resultRef;
 }
+
